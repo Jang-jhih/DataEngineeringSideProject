dbeb4d5bff98c5bf647ab027e9d8dcfa69808e51
Jacob <87475035+Jang-jhih@users.noreply.github.com>
2024-03-04 07:22:00 +0800

    更新配置和內容。

--- _start_all.sh
+++ _start_all.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#stop all containers
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+
+# sudo chmod -R 777 /var/run/docker.sock
+
+docker network rm my_network
 docker network create my_network
 
 
@@ -15,17 +23,14 @@ docker compose -f ./jupyter/docker-compose.yml up -d
 #superset
 docker compose -f ./superset/docker-compose.yml up -d
 
+#datahub
+docker compose -f ./datahub/docker-compose.yml up -d
 
-#stop all containers
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
 
-docker network rm my_network
-docker network create my_network
+
 # sudo service docker restart
 # docker compose logs -f spark-worker-1
 # sudo service docker restart
 
 
-# sudo chmod -R 777 /var/run/docker.sock
+# 
