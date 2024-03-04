b2be2bffa0aeea03f5ad382aeae292954964e0ab
Jacob <87475035+Jang-jhih@users.noreply.github.com>
2024-03-05 07:25:17 +0800

    更新配置和內容。

--- superset/docker/docker-bootstrap.sh
+++ superset/docker/docker-bootstrap.sh
@@ -23,7 +23,7 @@ REQUIREMENTS_LOCAL="/app/docker/requirements-local.txt"
 if [ "$CYPRESS_CONFIG" == "true" ]; then
     export SUPERSET_CONFIG=tests.integration_tests.superset_test_config
     export SUPERSET_TESTENV=true
-    export SUPERSET__SQLALCHEMY_DATABASE_URI=postgresql+psycopg2://superset:superset@db:5432/superset
+    export SUPERSET__SQLALCHEMY_DATABASE_URI=postgresql+psycopg2://postgres:postgres@db:5432/superset
 fi
 #
 # Make sure we have dev requirements installed
