c66230bd4f066aa80a1aa91009023021775e2c28
Jacob <87475035+Jang-jhih@users.noreply.github.com>
2024-03-05 07:51:42 +0800

    更新配置和內容。

---
+++ start_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 定義 Docker Compose 文件所在的目錄
+directories=(
+    "volume"
+    "superset"
+    "queue"
+    "jupyter"
+    "hadoop"
+    "datahub"
+)
+
+# 在每個目錄中運行 docker-compose up -d
+for dir in "${directories[@]}"; do
+    echo "Starting services in $dir..."
+    docker-compose -f "$dir/docker-compose.yml" up -d
+    if [ $? -ne 0 ]; then
+        echo "Failed to start services in $dir"
+        exit 1
+    fi
+done
+
+echo "All services started successfully."
